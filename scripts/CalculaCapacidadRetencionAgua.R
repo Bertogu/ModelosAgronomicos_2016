8b4b48b9d7db83d28eb7b20e8b873b18711a4972
unknown <ita-gutgaral@JAGITA46W106.itacyl.red>
2015-09-29 14:24:16 +0200

    Formulas para el cálculo de FC, WP, Permeabilidad. TODO: Hacerlo con imagenes

--- scripts/CalculaCapacidadRetencionAgua.R
+++ scripts/CalculaCapacidadRetencionAgua.R
@@ -18,22 +18,44 @@ library(ggplot2)
 # library(e1071)
 library(maptools)
 
-
+### Carga las funciones 
 source("scripts/funcionesComunes.R")
-CargaimagenesDeTextura()
+CargaimagenesDeTextura() # Load texture interpolated maps and work with them. More info in the function
 
-#TODO: Remove 0 values from clay 
-a<-preparaDatos()
-str(a)
+Textura<-preparaDatos() # Load information into the structure to work with
+str(Textura)
 
-writeGDAL(fname="ImagesOut/Arena_borrar.tif",drivername="GTiff",options="TFW=YES",dataset=a[5])
-writeGDAL(fname="ImagesOut/Limo_borrar.tif",drivername="GTiff",options="TFW=YES",dataset=a[6])
-writeGDAL(fname="ImagesOut/Clay_borrar.tif",drivername="GTiff",options="TFW=YES",dataset=a[7])
+########################################
+## Formulas for FC, WP and Permeability
+########################################
 
+Arena_Porc<-98
+Arcilla_Porc<-0
+MO_Porc<-0.83
 
+# Field Capacity
+CC_Prov<- -0.251*Arena_Porc/100+0.195*Arcilla_Porc/100+0.011*MO_Porc+0.006*Arena_Porc/100*Arcilla_Porc/100-0.027*Arcilla_Porc/100*MO_Porc+0.452*Arena_Porc/100*Arcilla_Porc/100+0.299
+CC_Vol<-CC_Prov+(1.283*CC_Prov*CC_Prov-0.374*CC_Prov-0.015)
 
+# Wilting point
+PM_Prov<- -0.024*Arena_Porc/100+0.487*Arcilla_Porc/100+0.006*MO_Porc+0.005*Arena_Porc/100*MO_Porc-0.013*Arcilla_Porc/100*MO_Porc+0.068*Arena_Porc/100*Arcilla_Porc/100+0.031
+#PM_Vol<-PM_Prov+PM_Prov*0.14-0.02 # if (PM_Vol < 0) {PM_Prov<-0.017545} 
 
+if ((PM_Prov+PM_Prov*0.14-0.02)<0){
+    PM_Vol<-0.017545+0.017545*0.14-0.02
+} else{
+    PM_Vol<-PM_Prov+PM_Prov*0.14-0.02
+}
+    
 
+# Permeabilidad
+Perme_1<- 0.278*Arena_Porc/100+0.034*Arcilla_Porc/100+0.022*MO_Porc-0.018*Arena_Porc/100*MO_Porc-0.027*Arcilla_Porc/100*MO_Porc-0.584*Arena_Porc/100*Arcilla_Porc/100+0.078
+Perme_2<- 1-(1-(Perme_1+0.636*Perme_1-0.107+CC_Prov+(1.283 *CC_Prov*CC_Prov-0.374*CC_Prov-0.015)-0.097*Arena_Porc/100+0.043))-(CC_Prov+(1.283*CC_Prov*CC_Prov-0.374*CC_Prov-0.015))
+Perme_3<- (log(CC_Prov+(1.283*CC_Prov*CC_Prov-0.374*CC_Prov-0.015))-log(PM_Vol))/(log(1500)-log(33))
+
+Perme_mm_h<- 1930*Perme_2^(3-Perme_3)
+
+#############################################
 
 
 
